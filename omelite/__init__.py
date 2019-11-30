2f3a2f1aa6789f04ecbd608dbedffffcdcfcbbae
Talley Lambert <talley.lambert@gmail.com>
2019-11-30 12:09:24 -0500

    move things around, add ome.xsd

--- omelite/__init__.py
+++ omelite/__init__.py
@@ -1,9 +1,5 @@
 __version__ = "0.1.0"
 
-from .channel import Channel
-from .experimenter import Experimenter
-from .image import Image
-from .plane import Plane
-from .dataset import Dataset
+from model import Channel, Experimenter, Image, Plane, Dataset
 
 __all__ = ["Channel", "Image", "Plane", "Experimenter", "Dataset"]
---
+++ omelite/model/__init__.py
@@ -0,0 +1,7 @@
+from .channel import Channel
+from .experimenter import Experimenter
+from .image import Image
+from .plane import Plane
+from .dataset import Dataset
+
+__all__ = ["Channel", "Image", "Plane", "Experimenter", "Dataset"]
