ee9966eb15d90067ae09ed720776291b75de52ef
Talley Lambert <talley.lambert@gmail.com>
2020-07-21 19:47:14 -0400

    add --nogen option to run tests on pregenerated code (#14)
    * add --nogen option to run tests on pregenerated code
    
    * remove return

--- testing/test_autogen.py
+++ testing/test_autogen.py
@@ -2,20 +2,26 @@ import importlib
 import os
 import sys
 from glob import glob
+from pathlib import Path
 
 import pytest
 from xmlschema.validators.exceptions import XMLSchemaValidationError
 
-from ome_autogen import convert_schema
-from ome_types import from_xml
-from pathlib import Path
-
-
 TESTING_DIR = Path(__file__).parent
+SRC_MODEL = TESTING_DIR.parent / "src" / "ome_types" / "model"
 
 
 @pytest.fixture(scope="session")
-def model(tmp_path_factory):
+def model(tmp_path_factory, request):
+    if request.config.getoption("--nogen"):
+        if not SRC_MODEL.exists():
+            raise RuntimeError(
+                f"Please generate local {SRC_MODEL} before using --nogen"
+            )
+        sys.path.insert(0, str(SRC_MODEL.parent))
+        return importlib.import_module(SRC_MODEL.name)
+    from ome_autogen import convert_schema
+
     target_dir = tmp_path_factory.mktemp("test_model")
     xsd = TESTING_DIR / "ome-2016-06.xsd"
     convert_schema(url=xsd, target_dir=target_dir)
@@ -54,6 +60,7 @@ def mark_xfail(fname):
         ),
     )
 
+
 def true_stem(p):
     return p.name.partition(".")[0]
 
@@ -66,6 +73,8 @@ params = [
 
 @pytest.mark.parametrize("xml", params, ids=true_stem)
 def test_convert_schema(model, xml):
+    from ome_types import from_xml
+
     if true_stem(xml) in SHOULD_RAISE:
         with pytest.raises(XMLSchemaValidationError):
             assert from_xml(xml, model.OME)
