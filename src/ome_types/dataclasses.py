78ca96305922e4257fbe70cbc6f4e53e786a214a
Talley Lambert <talley.lambert@gmail.com>
2020-08-06 09:24:21 -0400

    improved repr (#26)
    * improved repr
    
    * ignore
    
    * update repr
    
    * just docs
    
    * change comma
    
    * fix coverage config

--- src/ome_types/dataclasses.py
+++ src/ome_types/dataclasses.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Callable, Optional, Type, Union
+from dataclasses import MISSING, fields
+from datetime import datetime
+from enum import Enum
+from textwrap import indent
+from typing import TYPE_CHECKING, Any, Callable, Optional, Sequence, Type, Union
 
 from pydantic import validator
 from pydantic.dataclasses import _process_class
@@ -13,6 +17,11 @@ EMPTY = object()
 
 @validator("id", pre=True, always=True)
 def validate_id(cls: Type[Any], value: Any) -> str:
+    """Pydantic validator for ID fields in OME dataclasses.
+
+    If no value is provided, this validator provides and integer ID, and stores the
+    maximum previously-seen value on the class.
+    """
     from typing import ClassVar, Union
 
     # get the required LSID type from the annotation
@@ -23,6 +32,7 @@ def validate_id(cls: Type[Any], value: Any) -> str:
     if not id_type:
         return value
 
+    # Store the highest seen value on the class._max_id attribute.
     if not hasattr(cls, "_max_id"):
         cls._max_id = 0
         cls.__annotations__["_max_id"] = ClassVar[int]
@@ -46,6 +56,11 @@ def validate_id(cls: Type[Any], value: Any) -> str:
 
 
 def modify_post_init(_cls: Type[Any]) -> None:
+    """Modify __post_init__.
+
+    Provides support for non-default arguments in dataclass subclasses (where the super
+    class has default args) by providing the default value "EMPTY" from this module.
+    """
     origin_post_init = getattr(_cls, "__post_init__", None)
     required_fields = {k for k, v in _cls.__dict__.items() if v is EMPTY}
 
@@ -63,11 +78,53 @@ def modify_post_init(_cls: Type[Any]) -> None:
     setattr(_cls, "__post_init__", new_post_init)
 
 
+def modify_repr(_cls: Type[Any]) -> None:
+    """Improved dataclass repr function.
+
+    Only show non-default values, and summarize containers.
+    """
+    # let classes still create their own
+    if _cls.__repr__ is not object.__repr__:
+        return
+
+    def new_repr(self: Any) -> str:
+        name = self.__class__.__qualname__
+        lines = []
+        for f in sorted(fields(self), key=lambda f: f.name not in ("name", "id")):
+            # https://github.com/python/mypy/issues/6910
+            if f.default_factory is not MISSING:  # type: ignore
+                default = f.default_factory()  # type: ignore
+            else:
+                default = f.default
+
+            current = getattr(self, f.name)
+            if current != default:
+                if isinstance(current, Sequence) and not isinstance(current, str):
+                    rep = f"[<{len(current)} {f.name.title()}>]"
+                elif isinstance(current, Enum):
+                    rep = repr(current.value)
+                elif isinstance(current, datetime):
+                    rep = f"datetime.fromisoformat({current.isoformat()!r})"
+                else:
+                    rep = repr(current)
+                lines.append(f"{f.name}={rep},")
+        if len(lines) == 1:
+            body = lines[-1].rstrip(",")
+        elif lines:
+            body = "\n" + indent("\n".join(lines), "   ") + "\n"
+        else:
+            body = ""
+        out = f"{name}({body})"
+        return out
+
+    setattr(_cls, "__repr__", new_repr)
+
+
 def ome_dataclass(
     _cls: Optional[Type[Any]] = None,
     *,
     init: bool = True,
-    repr: bool = True,
+    repr: bool = False,
     eq: bool = True,
     order: bool = False,
     unsafe_hash: bool = False,
@@ -86,7 +143,8 @@ def ome_dataclass(
                 setattr(cls, "id", None)
 
         modify_post_init(cls)
-
+        if not repr:
+            modify_repr(cls)
         return _process_class(cls, init, repr, eq, order, unsafe_hash, frozen, config)
 
     return wrap if _cls is None else wrap(_cls)
