16ee59d058a1e7dbaf6094501cad248c209c64f5
Talley Lambert <talley.lambert@gmail.com>
2020-09-09 14:19:53 -0400

    import .schema after .model

--- src/ome_types/__init__.py
+++ src/ome_types/__init__.py
@@ -2,13 +2,10 @@ import os
 from pathlib import Path
 from typing import Union
 
-from .schema import to_dict, validate
-
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-
 
 try:
     from .model import OME
@@ -17,6 +14,8 @@ except ImportError:
         "Could not import 'ome_types.model.OME'.\nIf you are in a dev environment, "
         "you may need to run 'python -m src.ome_autogen'"
     ) from None
+from .schema import to_dict, validate  # isort:skip
+
 
 __all__ = ["to_dict", "validate", "from_xml"]
 
