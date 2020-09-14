546f20fd6141bd2503e874c03f1831aefa27ce4b
Talley Lambert <talley.lambert@gmail.com>
2020-09-14 16:34:12 -0400

    remove comment

--- testing/test_model.py
+++ testing/test_model.py
@@ -24,7 +24,6 @@ SHOULD_FAIL_ROUNDTRIP = {
     "timestampannotation-posix-only",
     "transformations-downgrade",
     "transformations-upgrade",
-    # type Enum not being converted to str
 }
 SKIP_ROUNDTRIP = {
     # These have XMLAnnotations with extra namespaces and mixed content, which
