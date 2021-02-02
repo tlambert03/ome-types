70e4e85d609a69810b7b47f798a432ce77c4dae9
Talley Lambert <talley.lambert@gmail.com>
2021-02-02 13:59:09 -0500

    quick try

---
+++ .github/scripts/download_aics_test_data.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from quilt3 import Package
+
+
+def download_test_resources() -> None:
+    root = Path(__file__).parent.parent.parent
+    resources = (root / "aicsimageio" / "tests" / "resources").resolve()
+    resources.mkdir(exist_ok=True)
+    package = Package.browse(
+        "aicsimageio/test_resources", "s3://aics-modeling-packages-test-resources"
+    )
+    package["resources"].fetch(resources)
+
+
+if __name__ == "__main__":
+    download_test_resources()
