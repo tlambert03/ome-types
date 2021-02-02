a558215124d5d54206102e9bd8a280cf2ad1c739
Talley Lambert <talley.lambert@gmail.com>
2021-02-02 15:41:52 -0500

    cache

--- .github/scripts/download_aics_test_data.py
+++ .github/scripts/download_aics_test_data.py
@@ -5,9 +5,7 @@ from quilt3 import Package
 
 def download_test_resources() -> None:
     root = Path(__file__).parent.parent.parent
-    print("root is", root)
     resources = (root / "aicsimageio" / "aicsimageio" / "tests" / "resources").resolve()
-    print("resources is", resources)
     resources.mkdir(exist_ok=True)
     package = Package.browse(
         "aicsimageio/test_resources", "s3://aics-modeling-packages-test-resources"
