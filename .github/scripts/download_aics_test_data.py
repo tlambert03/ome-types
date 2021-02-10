ea4558a687add98d3d1c2a4bd9dd212bed8d76f7
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-02-09 18:10:25 -0800

    Use AICS test resource top hash

--- .github/scripts/download_aics_test_data.py
+++ .github/scripts/download_aics_test_data.py
@@ -6,9 +6,17 @@ from quilt3 import Package
 def download_test_resources() -> None:
     root = Path(__file__).parent.parent.parent
     resources = (root / "aicsimageio" / "aicsimageio" / "tests" / "resources").resolve()
+
+    # Get the specific hash for test resources
+    with open(root / "aicsimageio" / "scripts" / "TEST_RESOURCES_HASH.txt", "r") as f:
+        top_hash = f.readline()
+
+    # Download test resources
     resources.mkdir(exist_ok=True)
     package = Package.browse(
-        "aicsimageio/test_resources", "s3://aics-modeling-packages-test-resources"
+        "aicsimageio/test_resources",
+        "s3://aics-modeling-packages-test-resources",
+        top_hash=top_hash,
     )
     package["resources"].fetch(resources)
 
