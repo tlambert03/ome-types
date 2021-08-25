0357e5d9c46dc01c2eaf1d0d2f86f1a8a872f519
Talley Lambert <talley.lambert@gmail.com>
2021-08-25 07:56:30 -0400

    only test on linux

--- testing/test_bioformats.py
+++ testing/test_bioformats.py
@@ -1,9 +1,16 @@
+import os
+import sys
 from pathlib import Path
+
+import pytest
 
 from ome_types import from_bioformats, from_tiff
 
+if os.getenv("CI") and not sys.platform.startswith("linux"):
+    pytest.skip("not on linux, skipping bioformats", allow_module_level=True)
 
-def test_bioformats():
+
+def test_bioformats() -> None:
     data = Path(__file__).parent / "data" / "ome.tiff"
     ome = from_bioformats(data)
     ome2 = from_tiff(data)
@@ -11,7 +18,7 @@ def test_bioformats():
     assert ome.uuid == ome2.uuid
 
 
-def test_bioformats2():
+def test_bioformats2() -> None:
     # for CI, this should grab a local copy
     data = Path(__file__).parent / "data" / "ome.tiff"
     ome = from_bioformats(data)
