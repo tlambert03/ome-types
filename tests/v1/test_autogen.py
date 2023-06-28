12a441683bc50dc644ccb1f9d853fe4909aeb906
Talley Lambert <talley.lambert@gmail.com>
2023-06-28 09:59:10 -0400

    fix tests

--- tests/v1/test_autogen.py
+++ tests/v1/test_autogen.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 import pytest
 
-ome_autogen = pytest.importorskip("ome_autogen")
+ome_autogen = pytest.importorskip("_ome_autogen")
+XSD = Path(__file__).parent.parent.parent / "src" / "ome_types" / "ome-2016-06.xsd"
 
 
 def test_autogen(tmp_path_factory):
@@ -13,8 +14,7 @@ def test_autogen(tmp_path_factory):
     This does *not* actually test the resulting model.
     """
     target_dir = tmp_path_factory.mktemp("_ome_types_test_model")
-    xsd = Path(__file__).parent.parent / "src" / "ome_types" / "ome-2016-06.xsd"
-    ome_autogen.convert_schema(url=xsd, target_dir=target_dir)
+    ome_autogen.convert_schema(url=XSD, target_dir=target_dir)
     sys.path.insert(0, str(target_dir.parent))
     assert importlib.import_module(target_dir.name)
     sys.path.pop(0)
