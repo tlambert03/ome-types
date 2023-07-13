207fd0268946704215d99b9a004efb82212d0e91
Talley Lambert <talley.lambert@gmail.com>
2023-07-13 11:55:07 -0400

    ci: add benchmarks

--- tests/test_codspeed.py
+++ tests/test_codspeed.py
@@ -2,13 +2,18 @@ from __future__ import annotations
 
 import sys
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import pytest
 
-from ome_types import from_tiff, from_xml, to_xml
+from ome_types import OME, from_tiff, from_xml, to_dict, to_xml
 
 if all(x not in {"--codspeed", "tests/test_codspeed.py"} for x in sys.argv):
     pytest.skip("use --codspeed to run benchmarks", allow_module_level=True)
+
+if TYPE_CHECKING:
+    from pytest_codspeed.plugin import BenchmarkFixture
+
 
 DATA = Path(__file__).parent / "data"
 TIFF = DATA / "ome.tiff"  # 1KB
@@ -25,7 +30,7 @@ def test_time_from_xml(file: Path) -> None:
 
 
 @pytest.mark.parametrize("file", XML, ids=["small", "med", "large"])
-def test_time_to_xml(file: Path, benchmark) -> None:
+def test_time_to_xml(file: Path, benchmark: BenchmarkFixture) -> None:
     ome = from_xml(file)
     benchmark(lambda: to_xml(ome))
 
@@ -33,3 +38,14 @@ def test_time_to_xml(file: Path, benchmark) -> None:
 @pytest.mark.benchmark
 def test_time_from_tiff() -> None:
     _ = from_tiff(TIFF)
+
+
+@pytest.mark.parametrize("file", [SMALL, MED], ids=["small", "med"])
+def test_time_from_xml_to_dict(file: Path) -> None:
+    _ = to_dict(file)
+
+
+@pytest.mark.parametrize("file", [SMALL, MED], ids=["small", "med"])
+def test_time_from_dict_to_ome(file: Path, benchmark: BenchmarkFixture) -> None:
+    d = to_dict(file)
+    benchmark(lambda: OME(**d))
