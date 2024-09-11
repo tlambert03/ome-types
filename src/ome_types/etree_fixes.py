41d4280e19b8d17bc06f5b0b1e1954ff41554364
Talley Lambert <talley.lambert@gmail.com>
2024-09-11 15:54:55 -0400

    lint

--- src/ome_types/etree_fixes.py
+++ src/ome_types/etree_fixes.py
@@ -23,14 +23,15 @@ def fix_micro_manager_instrument(tree: AnyElementTree) -> AnyElementTree:
     the warning from being raised.
     """
     for i_idx, instrument in enumerate(tree.findall("Instrument", NSMAP)):
-        old_id = instrument.get("ID")
+        old_id = instrument.get("ID")  # type: ignore [attr-defined]
         if old_id.startswith("Microscope"):
             new_id = f"Instrument:{i_idx}"
-            instrument.set("ID", new_id)
+            instrument.set("ID", new_id)  # type: ignore [attr-defined]
             for ref in tree.findall(f".//InstrumentRef[@ID='{old_id}']", NSMAP):
                 ref.set("ID", new_id)
 
-        for d_idx, detector in enumerate(instrument.findall(".//Detector", NSMAP)):
+        detectors = instrument.findall(".//Detector", NSMAP)  # type: ignore [attr-defined]
+        for d_idx, detector in enumerate(detectors):
             old_id = detector.get("ID")
             if not old_id.startswith("Detector:"):
                 new_id = f"Detector:{old_id if old_id.isdigit() else d_idx}"
